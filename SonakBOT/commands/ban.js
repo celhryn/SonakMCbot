20096a53e7276154dcc3937b69c75adbdef68c78
celhryn <79858410+celhryn@users.noreply.github.com>
2023-02-19 23:11:48 +0100

    Update ban.js

--- SonakBOT/commands/ban.js
+++ SonakBOT/commands/ban.js
@@ -1,30 +1,29 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const config = require("./config.json");
+const token = config.token;
 
 client.on("message", message => {
-  if (message.content.startsWith("$help")) {
-    if (message.member.hasPermission("ADMINISTRATOR")) {
-      const embed = new Discord.MessageEmbed()
-        .setTitle("Liste des commandes")
-        .setDescription("Voici la liste des commandes disponibles :")
-        .addField("$ban [utilisateur] [raison]", "Bannir un membre du serveur")
-        .addField("$kick [utilisateur] [raison]", "Expulser un membre du serveur")
-        .addField("$addrole [utilisateur] [rôle]", "Ajouter un rôle à un membre du serveur")
-        .addField("$mute [utilisateur]", "Muter un membre du serveur")
-        .addField("$say [message]", "Envoyer un message en tant que bot")
-        .addField("$announce [message] [ping (optionnel)] [image (optionnel)]", "Envoyer une annonce en embed message avec option de ping et image")
-        .setColor("#0099ff")
-        .setThumbnail(message.guild.iconURL())
-        .setFooter("Bot SonakMC développé par Celhryn");
-      message.channel.send(embed);
+  if (message.content.startsWith("$ban")) {
+    if (message.member.hasPermission("BAN_MEMBERS")) {
+      const member = message.mentions.members.first();
+      if (!member) {
+        return message.reply("Veuillez mentionner un membre valide de ce serveur");
+      }
+      const reason = message.content.slice(5 + member.toString().length);
+      if (!reason) {
+        return message.reply("Veuillez indiquer une raison pour le bannissement");
+      }
+      member.ban({ reason: reason })
+        .then(() => {
+          message.reply(`${member.user.tag} a été banni avec succès.`);
+        })
+        .catch(error => {
+          console.error(`Une erreur est survenue lors du bannissement de ${member.user.tag} :`, error);
+          message.reply("Une erreur est survenue lors du bannissement de ce membre");
+        });
     } else {
       message.reply("Vous n'avez pas la permission d'utiliser cette commande.");
     }
   }
 });
 
-client.login(MTA3MzM5NjQxMDM0MDM1NjE3Ng.GKWUJ9.xx-qdmUT4rzaU-_ViJ3cFHUDnEgkE2GLpg3QAY);
+client.login(token);
